e6324dbeff7ec95d7f68baf879483ec4e61f6588
AdeevMardia2008 <71126451+AdeevMardia2008@users.noreply.github.com>
2022-09-06 21:20:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,8 +23,19 @@ app.post("/send-mail", (req, res) => {
     const mailData = {
         from: "",
         to: to,
-        subject: prompt("Enter the subject of the email:"),
-        html: `hey there! checking this out`
+        subject: "Your payment is due!",
+        html: ` <p>
+                    Hello ${name},
+                </p>
+                <p>
+                    This is a reminder email that your payment of amount - ${amount} is due on date - ${date}
+                </p>
+                <p>
+                    Kindly make the payment before the due date to avoid any inconvenience.
+                </p>
+                <p>
+                    Thanks and Regards,
+                </p>`
     };
     transporter.sendMail(mailData, (error, info) => {
         if (error) {
